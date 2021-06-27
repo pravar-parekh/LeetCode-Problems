3a698718006b6cf890710fdddd9ab5ba8da62d1a
Pravar Parekh <pparekh3@asu.edu>
2021-06-27 16:07:46 -0700

    Update remove-nth-node-from-end-of-list.py

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -3,6 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+    **** TWO PASS SOLUTION ****
+    Params:- 
+        head : ListNode
+            Head of the Linked List
+            
+        n : int
+            The nth number index to remove from the end of the linked list
+            
+    Returns:-
+        dummy.next : Listnode
+            Head of the linked list
+    
+    Summary :-
+        The TWO-Pass solution first goes through the entire list to find total number elements.
+        Then it calculates n_ the position of the element to remove. It moves to the element previous
+        to n_ element and changes its next to skip the n_ element. If element head is removed then it 
+        simply returns head.next or if only 1 element then it returns None.          
+'''
+
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         total = 0
