60eaa1993a5f681a672bb9ab26487ea200f02fa5
Pravar Parekh <pparekh3@asu.edu>
2021-06-27 17:07:48 -0700

    Update middle-of-the-linked-list.py

--- middle-of-the-linked-list/middle-of-the-linked-list.py
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -3,6 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+    ****FAST AND SLOW POINTER****
+    
+    Params:-
+        head : ListNode
+            Head of the input list.
+    
+    Returns:-
+        ListNode
+            The middle node
+    
+    Summary:-
+        We keep a pointer that moves single step at a time and one that moves double steps 
+        at a time. When the double step pointer reaches the end, the single step will reach
+        the middle.
+        
+'''
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
         single = head
