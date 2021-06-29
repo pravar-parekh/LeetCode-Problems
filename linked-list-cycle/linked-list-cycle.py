d9942d53ea77a32c363e29b55ebfa4f9349517f6
Pravar Parekh <pparekh3@asu.edu>
2021-06-29 00:32:12 -0700

    Update linked-list-cycle.py

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -3,7 +3,21 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+'''
+    ****Tortoise and Hare Method****
+    Params:-
+        head : ListNode
+            Head of the list
+    
+    Returns:-
+        Bool
+            True if List has cycle else false
+    
+    Summary:- 
+        Use the Tortoise and Hare algorithm, i.e. a fast and a slow pointer. Fast moves
+        2x the speed of the slow pointer. If the two ever meet before the fast reaches
+        None, then it has cycle else no cycle.
+'''
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         slow = head
