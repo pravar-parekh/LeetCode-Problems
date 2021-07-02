ac7f922442281e82a0b29e994c4dcda800d570e3
Pravar Parekh <pparekh3@asu.edu>
2021-07-01 23:25:25 -0700

    Update intersection-of-two-linked-lists.py

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -3,7 +3,28 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+'''
+    Params:- 
+        headA : ListNode
+            Head of the list A
+        
+        headB : ListNode
+            Head of the list B
+    
+    Returns:-
+        ListNode
+            The node at which intersection happens or None
+    
+    Summary:-
+        We take two pointers and iterate through both the list in a loop which ends if 
+        they are equal. Since the lengths of both lists could be different, one could reach
+        end before the other. When that happens we simply set the pointer to the Head of the
+        other list. This will also occur with the pointer of the other list. But when the
+        second pointer is changed to the head of the first list, the other pointer would
+        already be ahead by the difference of their lengths in the shorter list. Thus they now
+        should meet at the intersection or reach Ends together, thus ending the loop. Dry run
+        this to further understand.
+'''
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         
