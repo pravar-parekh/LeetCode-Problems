2e1f95e0fd6d2cd185d1731fe7cae26f0079251e
Pravar Parekh <pparekh3@asu.edu>
2021-07-06 22:20:12 -0700

    Update rotate-array.py

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -1,3 +1,15 @@
+'''
+    Params:-
+        nums : List[int]
+            Array of Integers
+        k : int
+            Rotate the array by k
+            
+    Summary:-
+        Create a new k which is mod of n. This removes the possibility of cycling i.e. complete
+        360 rotation. Find the spot of the new first number by subtracting k from total length
+        of the array. Then split and replace the new array.
+'''
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -8,4 +20,3 @@ class Solution:
         new_i = n - 1 - k
         
         nums[:] = nums[new_i + 1:] + nums[:new_i+1]
-    
