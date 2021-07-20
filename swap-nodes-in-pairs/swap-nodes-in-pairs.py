489b01f46f6b6daddf2ac44a08d86919b46d546a
Pravar Parekh <pparekh3@asu.edu>
2021-07-20 00:53:18 -0700

    Update swap-nodes-in-pairs.py

--- swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -3,6 +3,13 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+Summary:- 
+    Create a dummy node to start from. You then swap it's next and next.next. You simply move to 
+    next.next. Now we could have done this without dummy node but then we would have to maitain the
+    previous node. Dummy node eleminates that need, because we always change the next 2 elements. 
+    So the element we are at acts as the previous node.
+'''
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
         
