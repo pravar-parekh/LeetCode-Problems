dd05cd7f61945301201e9ee6e56150ec0c9f98cf
Pravar Parekh <pparekh3@asu.edu>
2021-07-25 18:02:44 -0700

    Update linked-list-cycle-ii.py

--- linked-list-cycle-ii/linked-list-cycle-ii.py
+++ linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -3,11 +3,21 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+'''
+Summary:- 
+    Use Floyd Warshall's algorithm (Fast and slow Pointer).
+    
+    First detect a cycle present or not, using slow and fast pointers. If they become None, then no
+    cycle. If they become equal then it means that there is cycle. 
+    
+    Once its detected, then put the slow pointer to head. And then move slow and fast, only by 1 steps.
+    They meet at the start of the cycle.
+    
+    Explanation here: https://www.geeksforgeeks.org/find-first-node-of-loop-in-a-linked-list/
+'''
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        
-        
+
         if not head or not head.next:
             return None
         
