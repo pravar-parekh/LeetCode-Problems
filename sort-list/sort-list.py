f3ca01e07967edf4791cb792625ec41cb7ac11d5
Pravar Parekh <pparekh3@asu.edu>
2021-07-27 13:46:28 -0700

    Time: 484 ms (51.24%), Space: 30.3 MB (28.26%) - LeetHub

--- sort-list/sort-list.py
+++ sort-list/sort-list.py
@@ -6,41 +6,31 @@
 
 """
 Summary:-
-    Sort of a cheat approach. Simply copy the list to an array and sort the array. This
-    is apparently done faster than sorting the list itself. This is due to how array is
-    cached. So its way better to do it this way. Not sure if this should be done in an
-    interview.
-    
-    https://leetcode.com/problems/insertion-sort-list/discuss/46429/Thoughts-from-a-Google-interviewer
-    
-    "You could for sure talk about it in the interview and clear it out before jumping into    
-    sorting a list. But in the end, you end up doing what he wants you to do(Say he doesn't 
-    want you to use Extra Space).
-
-    Also, For example, you could talk about the following comparison of copying a list to array 
-    vs sorting the list : 
-    http://stackoverflow.com/questions/1525117/whats-the-fastest-algorithm-for-sorting-a-linked-list.
-
-    In my opinion, it's better to just put all the pros/cons on the table, and ask him which 
-    one he actually needs."
+    Basic Merge Sort. Although this uses O(N) Space due to recursion. Bottom Up approach. We can change the
+    the dummy variable approach to merge the list in place.
 """
 class Solution:
+    def merge(self, h1, h2):
+        dummy = tail = ListNode(None)
+        while(h1 and h2):
+            if h1.val < h2.val:
+                tail.next, tail, h1 = h1, h1, h1.next
+        
+            else:
+                tail.next, tail, h2 = h2, h2, h2.next
+        
+        # Whichever list is remaining
+        tail.next = h1 or h2
+        return dummy.next
+        
+    
     def sortList(self, head: ListNode) -> ListNode:
-        
-        array = []
-        
-        cur = head
-        while(cur):
-            array.append(cur.val)
-            cur = cur.next
-            
-        array = sorted(array)
-        
-        cur = head
-        i = 0
-        while(cur):
-            cur.val = array[i]
-            cur = cur.next
-            i += 1
+        if not head or not head.next:
+            return head
 
-        return head
+        prev, slow, fast = None, head, head        
+        while(fast and fast.next):
+            prev, slow, fast = slow, slow.next, fast.next.next        
+        prev.next = None
+        
+        return self.merge(*map(self.sortList, (head, slow)))
