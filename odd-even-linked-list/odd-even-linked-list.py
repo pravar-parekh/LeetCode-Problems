75ac0da172bf4b8d839570a8e2b349dad27d3d5c
Pravar Parekh <pparekh3@asu.edu>
2021-08-05 17:14:23 -0700

    Update odd-even-linked-list.py

--- odd-even-linked-list/odd-even-linked-list.py
+++ odd-even-linked-list/odd-even-linked-list.py
@@ -3,10 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+Summary:- 
+    We only need to save which one would the first even indexed node. So that when we reach the
+    end of the odd nodes, we can attach the even head to it. After that we only next to reassign
+    all the next pointers to alternate pointers until we reach the end, at which we attach the
+    two lists.
+
+"""
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
         if not head:
             return head
+        
         first_eve = head.next
         
         cur_odd = head
