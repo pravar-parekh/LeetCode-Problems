10b19535914b12021936cbf4e98714a3c470288d
Pravar Parekh <pparekh3@asu.edu>
2022-03-07 14:12:42 -0700

    Time: 141 ms (9.18%), Space: 13.8 MB (77.27%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        reverseNum = 0
+        temp = x
+        while(temp > 0):
+            reverseNum = reverseNum*10 + temp%10
+            temp //= 10
+            print(temp)
+            
+        if reverseNum == x:
+            return True
+        
+        else:
+            return False
